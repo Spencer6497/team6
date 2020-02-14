37aca5a6f5b73e77dd3606a534f60f685138598b
Spencer6497 <peaces2@mymail.nku.edu>
2020-02-13 19:32:44 -0500

    commit

--- hermes/karma.conf.js
+++ hermes/karma.conf.js
@@ -28,14 +28,15 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
+    var express = require(‘express’);
+    var port = process.env.PORT || 3000;
+    var app = express();
+    app.get(‘/’, function (req, res) {
+ res.send(JSON.stringify({ Hello: ‘World’ }));
+  });
+  app.listen(port, function () {
+    console.log(`Example app listening on port !`);
+  });
   });
 };
-var express = require(‘express’);
-var port = process.env.PORT || 3000;
-var app = express();
-app.get(‘/’, function (req, res) {
- res.send(JSON.stringify({ Hello: ‘World’ }));
-});
-app.listen(port, function () {
-  console.log(`Example app listening on port !`);
-});
+
